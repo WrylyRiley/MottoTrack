f3f8daa761bd24a744e18ec315643646aff147b1
Zach Bauer <zbauer91@gmail.com>
2018-01-09 11:46:29 -0500

    added prettier-eslint so the linter would stop complaining
    added content to Posts.vue

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -2,14 +2,35 @@
   <div class="posts">
     <h1>Posts</h1>
 
+    <div v-for="post in posts" :key="post">
+      <p>
+        <span>
+          <b>{{ post.title }}</b>
+        </span>
+        <span>{{ post.description }}</span>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+  import PostsService from '@/services/PostService'
+
   export default {
     name: 'posts',
-    data() {
-      return {}
+    data () {
+      return {
+        posts: []
+      }
+    },
+    mounted () {
+      this.getPosts()
+    },
+    methods: {
+      async getPosts () {
+        const response = await PostsService.fetchPosts()
+        this.posts = response.data
+      }
     }
   }
 </script>
