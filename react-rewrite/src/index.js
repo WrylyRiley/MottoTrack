6c5f6ed8bd991609786e3aeb848c15de16dd25cc
Zach Bauer <zbauer91@gmail.com>
2018-01-22 15:41:10 -0500

    remove logo, shuffled a few files for easier readibility and referencing

--- react-rewrite/src/index.js
+++ react-rewrite/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// Split out some App.js functionality to make room for Router, per the react-translator assignment. Used that index.js as the template here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App/App.js'
+import { BrowserRouter as Router } from 'react-router-dom'
+import registerServiceWorker from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
+registerServiceWorker()
