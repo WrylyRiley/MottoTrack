111114fba4701074691d01fecee147b5e5a2f510
Zach Bauer <zbauer91@gmail.com>
2018-01-22 15:57:12 -0500

    imported dark theme

--- react-rewrite/src/components/App/App.js
+++ react-rewrite/src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './App.css'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Route, Link, Redirect, Switch } from 'react-router-dom'
 
 class App extends Component {
@@ -8,11 +7,7 @@ class App extends Component {
     return (
       <main>
         <Switch>
-          <Route>
-            <MuiThemeProvider>
-              {/* Insert material component here */}
-            </MuiThemeProvider>
-          </Route>
+          <Route>{/* Insert component here */}</Route>
         </Switch>
       </main>
     )
--- react-rewrite/src/index.js
+++ react-rewrite/src/index.js
@@ -1,14 +1,35 @@
 // Split out some App.js functionality to make room for Router, per the react-translator assignment. Used that index.js as the template here
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App/App.js'
+//  Most-parent component
+import App from './components/App/App.js' // Most-parent app, handles routing
 import { BrowserRouter as Router } from 'react-router-dom'
+// Important because reasons
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
+// Material-UI Wrapper
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+// Dark theme
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+// Theme gettersetter
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+// Hoping this works!
+// Wrapping the most-parent component in the MUI wrapper for DRYer code
+
+const WrappedApp = () => {
+  return (
+    // MUI Wrapper
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <App />
+    </MuiThemeProvider>
+  )
+}
 
 ReactDOM.render(
+  // Router component
   <Router>
-    <App />
+    <WrappedApp />
   </Router>,
   document.getElementById('root')
 )
